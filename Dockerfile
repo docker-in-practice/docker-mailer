1bd834914453a159634fe072e3747eabf137e060
Ian Miell <ian.miell@gmail.com>
2015-11-04 19:31:56 +0000

    simpler

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM debian
+FROM ubuntu:12.04
 RUN apt-get update && apt-get install -y mailutils ssmtp
 COPY mail.sh /mail.sh
 RUN chmod +x /mail.sh
--- mail.sh
+++ mail.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
-ROOT=${EMAIL}
-MESSAGE=${MESSAGE}
-SUBJECT=${SUBJECT}
-TO=${TO}
-AUTHUSER=${AUTHUSER:-$EMAIL}
-MAILHUB=${MAILHUB:-smtp.gmail.com:587}
-REWRITEDOMAIN=${REWRITEDOMAIN}
-HOSTNAME=${HOSTNAME:-smtp.gmail.com:587}
-USESTARTTLS=${USESTARTTLS:-YES}
-USETLS=${USETLS:-YES}
-AUTHPASS=${AUTHPASS}
-AUTHMETHOD=${AUTHMETHOD:-LOGIN}
-FROMLINEOVERRIDE=${FROMLINEOVERRIDE:-YES}
+ROOT="${EMAIL}"
+MESSAGE="${MESSAGE}"
+SUBJECT="${SUBJECT}"
+TO="${TO}"
+AUTHUSER="${AUTHUSER:-$EMAIL}"
+MAILHUB="${MAILHUB:-smtp.gmail.com:587}"
+USESTARTTLS="${USESTARTTLS:-YES}"
+AUTHPASS="${AUTHPASS}"
 
 if [[ $EMAIL = '' ]] || [[ $MESSAGE = '' ]] || [[ $TO = '' ]]
 then
-	echo 'usage: docker run -e EMAIL=<email address> -e AUTHPASS=<password> -e MESSAGE=<message> -e TO=<email to> [-e AUTHUSER=[$EMAIL]] [-e MAILHUB=[smtp.gmail.com:587]] [-e HOSTNAME=[smtp.gmail.com:587]] [-e USESTARTTLS=[YES]] [-e USETLS=[YES]] [-e AUTHMETHOD=[LOGIN]] [-e FROMLINEOVERRIDE=[YES]] -e [-e REWRITEDOMAIN=[]] dockerinpractice/docker-mailer'
+	echo 'usage: docker run -e EMAIL=<email address> -e AUTHPASS=<password> -e MESSAGE=<message> -e TO=<email to> [-e AUTHUSER=[$EMAIL]] [-e MAILHUB=[smtp.gmail.com:587]] [-e USESTARTTLS=[YES]] dockerinpractice/docker-mailer'
 	exit 1
 fi
 
 cat > /etc/ssmtp/ssmtp.conf << EOF
-root=${EMAIL}
 mailhub=${MAILHUB}
-rewriteDomain=${REWRITEDOMAIN}
-hostname=${HOSTNAME}
 UseSTARTTLS=${USESTARTTLS}
-UseTLS=${USETLS}
 AuthUser=${AUTHUSER}
 AuthPass=${AUTHPASS}
-AuthMethod=${AUTHMETHOD}
-FromLineOverride=${FROMLINEOVERRIDE}
 EOF
 
 echo "$SUBJECT" | mail -s "$MESSAGE" -t "$TO"
